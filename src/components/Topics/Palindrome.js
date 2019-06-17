27048c6db4517d9133fed161c9d53950a3c459d0
Tyler Hawkins <thawkinsusa>
2019-06-17 16:39:11 -0600

    second

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -7,35 +7,37 @@ export default class Palindrome extends Component {
 
     this.state = {
       userInput: '',
-      palindrome: ''
+      palindrome: '',
+      backwardSpelling: ''
     };
   }
 
-  handleChange(val) {
-    this.setState({ userInput: val });
-  }
 
-  isPalindrome(userInput) {
+  isReversable(userInput) {
     let forwards = userInput;
     let backwards = userInput;
     backwards = backwards.split('');
     backwards = backwards.reverse();
     backwards = backwards.join('');
-
+    console.log(backwards)
+    this.setState({backwardSpelling: backwards})
+   
     if ( forwards === backwards ) {
       this.setState({ palindrome: 'true' });
     } else {
       this.setState({ palindrome: 'false' });
     }
   }
-
+  handleChange(val) {
+    this.setState({ userInput: val });
+  }
   render() {
     return (
       <div className="puzzleBox palindromePB">
         <h4> Is it spelled the same Forwards to Backwards? </h4>
         <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
-        <button className="confirmationButton" onClick={ () => this.isPalindrome(this.state.userInput) }> Check </button>
-        <span className="resultsBox"> Forward Arrangement: { this.state.palindrome } </span>
+        <button className="confirmationButton" onClick={ () => this.isReversable(this.state.userInput) }> Check </button>
+        <span className="resultsBox"> Reversed: { JSON.stringify(this.state.backwardSpelling) }<br></br> Is is the same: { this.state.palindrome } </span>
       </div>
     )
   }
